1566c7c78e42bbeaa437d13c5a536dfb65a926fa
NipunaMadhushan <nipuna.madhushan96@gmail.com>
2025-01-09 15:25:39 +0530

    Update otlp versions

--- native/src/main/java/io/ballerina/observe/trace/newrelic/NewRelicTracerProvider.java
+++ native/src/main/java/io/ballerina/observe/trace/newrelic/NewRelicTracerProvider.java
@@ -39,7 +39,7 @@ import io.opentelemetry.sdk.trace.samplers.Sampler;
 
 import java.util.concurrent.TimeUnit;
 
-import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
+import static io.opentelemetry.semconv.ResourceAttributes.SERVICE_NAME;
 
 /**
  * This is the New Relic tracing extension class for {@link TracerProvider}.
@@ -107,7 +107,7 @@ public class NewRelicTracerProvider implements TracerProvider {
 
         return tracerProviderBuilder.setResource(
                         Resource.create(Attributes.of(SERVICE_NAME, serviceName)))
-                .build().get(TRACER_NAME);
+                .build().get("jaeger");
     }
 
     @Override
